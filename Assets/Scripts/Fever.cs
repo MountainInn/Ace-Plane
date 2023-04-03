d5f18d5be379ceb8ce283555fca04bd5510b2642
Mountain Inn <blckclst@gmail.com>
2023-04-03 17:11:28 +0500

    Add Fever

---
+++ Assets/Scripts/Fever.cs
@@ -0,0 +1,107 @@
+using System;
+
+public class Fever : MonoBehaviour
+{
+    CircleCollider2D trigger;
+    Image indicator;
+
+    float
+        secondsToFever,
+        feverDuration,
+        t;
+
+    Action<Collider2D> onTriggerStay;
+    Action update;
+
+    public event Action
+        onFeverStarted,
+        onFeverEnded;
+
+    private void Awake()
+    {
+        var gameSettings = GameSettings.Get();
+        secondsToFever = gameSettings.secondsToFever;
+        feverDuration = gameSettings.feverDuration;
+
+#if UNITY_EDITOR
+
+        gameObject.AddComponent<GameSettingsWatcher>()
+            .onUpdate += () =>
+            {
+                secondsToFever = gameSettings.secondsToFever;
+                feverDuration = gameSettings.feverDuration;
+            };
+#endif
+    }
+
+    private void Start()
+    {
+        SwitchToNormal();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.TryGetComponent(out Missile missile))
+        {
+            missilesInRange++;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.TryGetComponent(out Missile missile))
+        {
+            missilesInRange--;
+        }
+    }
+
+    private void SwitchToNormal()
+    {
+        t = 0;
+        update = NormalUpdate;
+
+        onFeverEnded?.Invoke();
+    }
+
+    private void NormalUpdate()
+    {
+        if (missilesInRange == 0)
+        {
+            t -= Time.deltaTime * .5f;
+        }
+        else if (missilesInRange > 0)
+        {
+            t += Time.deltaTime;
+        }
+
+        indicator.fillAmount = t / secondsToFever;
+
+        if (t >= secondsToFever)
+        {
+            SwitchToFever();
+        }
+    }
+
+    private void SwitchToFever()
+    {
+        t = feverDuration;
+        update = FeverUpdate;
+
+        onFeverStarted?.Invoke();
+    }
+
+    private void FeverUpdate()
+    {
+        t -= Time.deltaTime;
+
+        if (t <= 0)
+        {
+            SwitchToNormal();
+        }
+    }
+
+    private void Update()
+    {
+        update.Invoke();
+    }
+}
