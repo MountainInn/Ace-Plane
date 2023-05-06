769aafd7afc9c8d72e54b52d63933bf733ab9ec8
Mountain <blckclst@gmail.com>
2023-05-07 01:12:46 +0500

    add events on skin changes in Wardrobe

--- Assets/Scripts/Wardrobe.cs
+++ Assets/Scripts/Wardrobe.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenuAttribute(menuName="Wardrobe")]
@@ -5,8 +6,32 @@ public class Wardrobe : ScriptableObject
 {
     static public Wardrobe instance => _inst ??= Resources.Load<Wardrobe>("Wardrobe");
     static Wardrobe _inst;
-    public Skin
-        planeSkin,
-        engineParticleSkin;
+
+    private Skin _planeSkin;
+    private Skin _engineParticleSkin;
+
+    public Action onPlaneSkinChanged;
+    public Action onEngineParticleSkinChanged;
+
+    public Skin planeSkin
+    {
+        get => _planeSkin;
+        set
+        {
+            _planeSkin = value;
+            onPlaneSkinChanged?.Invoke();
+        }
+    }
+    public Skin engineParticleSkin
+    {
+        get => _engineParticleSkin;
+        set
+        {
+            _engineParticleSkin = value;
+
+            onEngineParticleSkinChanged?.Invoke();
+
+        }
+    }
 
 }
